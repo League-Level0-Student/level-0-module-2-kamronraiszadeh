b39086efcfe868d875180e357e438314a6db93db
kamronraiszadeh <league@WTS10.attlocal.net>
2018-01-17 16:58:34 -0800

    lottery numbers, validation IP

---
+++ src/random/LotteryNumbers.java
@@ -0,0 +1,19 @@
+package random;
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class LotteryNumbers {
+public static void main(String[] args) {
+	Random number= new Random () ;
+	int num1=number.nextInt(6);
+	int num2=number.nextInt(6);
+	int num3=number.nextInt(6);
+	int num4=number.nextInt(6);
+	int num5=number.nextInt(6);
+	JOptionPane.showMessageDialog(null,"Your lottery numbers are:"+num1+num2+num3+num4+num5);
+	
+	
+}
+}
